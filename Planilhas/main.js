1f7bcc0471334dcb75a83c4c8c7357e652e5103e
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2018-11-28 00:11:26 -0200

    Add files via upload

--- Planilhas/main.js
+++ Planilhas/main.js
@@ -1,7 +1,12 @@
 
-document.body.onscroll = function() {
-        document.querySelector('.price_tag').style.transform = "translateY( -" + (window.pageYOffset / 40) + "vh)"
+    document.querySelector('.price_tag').style.transform = "translateY( -" + (window.pageYOffset / 40) + "vh)"
 
-        document.querySelector('.planilha').style.transform = "translateY(" + (window.pageYOffset / 25) + "vh)"
-        
+    document.querySelector('.planilha').style.transform = "translateY(" + (window.pageYOffset / 25) + "vh)"   
+
+document.body.onscroll = function() {
+    document.querySelector('.price_tag').style.transform = "translateY( -" + (window.pageYOffset / 40) + "vh)"
+
+    document.querySelector('.planilha').style.transform = "translateY(" + (window.pageYOffset / 25) + "vh)"   
+    
+    document.querySelector('.bg2').style.transform = "translateY( -" + (window.pageYOffset / 25) + "vh)"   
 }
