75d42432471a6d4344867553c1e0e554829d00a9
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2018-11-28 14:30:57 -0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const header = document.querySelector('header')
-const h1 = document.querySelector('.h1_container h1')
-const h1Span = document.querySelector('.h1_container h1 span')
-const headerContactContainer = document.querySelector('.header_contact_container')
-const headerContactContainerSpan = document.querySelector('.header_contact_container span:first-child')
+const header = document.querySelector('header');
+const h1 = document.querySelector('.h1_container h1');
+const h1Span = document.querySelector('.h1_container h1 span');
+const headerContactContainer = document.querySelector('.header_contact_container');
+const headerContactContainerSpan = document.querySelector('.header_contact_container span:first-child');
+
+const goTop = document.querySelector('.go_top');
 
 document.body.onscroll = function() {
     if(window.pageYOffset > (window.innerHeight / 3)) {
@@ -11,14 +13,30 @@ document.body.onscroll = function() {
         h1Span.classList.add('h1Span_shrink')
         headerContactContainer.classList.add('header_contact_shrink')
         headerContactContainerSpan.classList.add('header_contact_span_shrink')
+        goTop.classList.add('go_top_show')
     } else {
         header.classList.remove('header_shrink')
         h1.classList.remove('h1_shrink')
         h1Span.classList.remove('h1Span_shrink')
         headerContactContainer.classList.remove('header_contact_shrink')
         headerContactContainerSpan.classList.remove('header_contact_span_shrink')
+        goTop.classList.remove('go_top_show')
+    }
+
+    
+}
+
+function goToTop() {
+    let calc = (- window.pageYOffset / 30) - 3
+    if (window.pageYOffset > 0) {
+        window.scrollBy(0, calc);
+        setTimeout(goToTop, 0);
     }
 }
+
+goTop.addEventListener('click', function() {
+    goToTop()
+})
 
 
 const aboutButton = document.querySelector('#aboutButton');
